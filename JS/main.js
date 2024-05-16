baeb83ec31477b2770ca1d7265db9da30fe9dc1b
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-16 15:05:55 +0200

    First Commit

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            titolo: "Vue-BoolZapp"
+        }
+    },
+
+    methods: {
+
+
+    },
+
+
+    //MOMENTI DI BASE
+    mounted() {
+        console.log("App montata")
+
+    }
+}).mount("#app")
